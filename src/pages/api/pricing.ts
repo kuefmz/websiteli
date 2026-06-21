9bf1b5802210c3a9ba7592d32328b164064e9c57
Jenifer Tabita Ciuciu-Kiss <jenifer.girl.98@gmail.com>
2026-06-21 23:48:33 +0200

    fixing pricing

--- src/pages/api/pricing.ts
+++ src/pages/api/pricing.ts
@@ -1,11 +1,12 @@
 import type { APIRoute } from "astro";
-import { getResolvedPricing } from "../../lib/pricing.server";
+import { getResolvedPricing, pricingVaryHeader } from "../../lib/pricing.server";
 
 export const GET: APIRoute = ({ request }) => {
   return new Response(JSON.stringify(getResolvedPricing(request)), {
     headers: {
       "Content-Type": "application/json",
       "Cache-Control": "private, no-store",
+      Vary: pricingVaryHeader,
     },
   });
 };
